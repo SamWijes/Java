62c12b70a39e27f0cd5070601b3435197dd9bf8a
Samith <samithmw1000@gmail.com>
2025-07-16 19:47:46 +0530

    JavaCss-Link s

--- src/main/java/org/example/javafxcss/HelloController.java
+++ src/main/java/org/example/javafxcss/HelloController.java
@@ -1,9 +1,15 @@
 package org.example.javafxcss;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelloController {
 
@@ -11,6 +17,8 @@ public class HelloController {
 	private Label label;
 	@FXML
 	private Button button4;
+	@FXML
+	private GridPane gridpane;
 
 	public void initialize() {
 		button4.setEffect(new DropShadow());
@@ -26,5 +34,23 @@ public class HelloController {
 		label.setScaleX(1.0);
 	}
 
+
+	public void handleClick() {
+		FileChooser chooser = new FileChooser();
+//		DirectoryChooser chooser=new DirectoryChooser();
+		chooser.setTitle("Save application File");
+		chooser.getExtensionFilters().addAll(
+				new FileChooser.ExtensionFilter("Text", "*.zip"),
+				new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+				new FileChooser.ExtensionFilter("All Files", "*.*"));
+		File file=chooser.showOpenDialog(gridpane.getScene().getWindow());
+		if (file != null) {
+			System.out.println(file.getPath());
+		}else System.out.println("Chooser cancelled");
+	}
+	@FXML
+	public void handleLinkClick(){
+		System.out.println("link CLicked");
+	}
 
 }
